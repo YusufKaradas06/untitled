a4880f427250ea564789ba1182e4f1fa7196450a
Yusuf <ysfkrds.63@gmail.com>
2023-04-08 12:49:43 +0200

    Slm.

---
+++ src/test/java/com/group_5/test/Deneme/deneme.java
@@ -0,0 +1,11 @@
+package com.group_5.test.Deneme;
+
+import com.group_5.test.TestBase;
+import org.testng.annotations.Test;
+
+public class deneme extends TestBase {
+    @Test
+    public void testName1() {
+        System.out.println("Hello World");
+    }
+}
